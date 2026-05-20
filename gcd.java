03efa1afa63bbe1625da3255e353f8299957642c
somnathsingh712 <somnathsingh9517@gmail.com>
2026-05-20 15:59:58 +0530

    gcd

---
+++ gcd.java
@@ -0,0 +1,14 @@
+public class gcd {
+    public static void main(String[] args) {
+       int a=8;
+       int b=12;
+       while(b!=0){
+        int temp=b;
+        b=a%b;
+        a=temp;
+
+       } 
+       System.out.println(a);
+
+    }
+}
