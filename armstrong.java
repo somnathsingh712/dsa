336554ea67950fc1c1aea338da1b1ba7592dcd5a
somnathsingh712 <somnathsingh9517@gmail.com>
2026-05-20 15:19:25 +0530

    armstrong number

---
+++ armstrong.java
@@ -0,0 +1,18 @@
+public class armstrong {
+    public static void main(String[] args) {
+        int n=153;
+        int temp=n;
+        int sum=0;
+        while(n!=0){
+            int digit=n%10;
+            sum+=digit*digit*digit;
+            n=n/10;
+        }
+        if(temp==sum){
+            System.out.println("Armstrong Number");
+        }
+        else{
+            System.out.println("Not an armstrong number");
+        }
+    }
+}
