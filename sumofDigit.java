6da324c6e17462cbdf80a36b4af78927e7a78201
somnathsingh712 <somnathsingh9517@gmail.com>
2026-05-20 15:26:13 +0530

    sum of digits

---
+++ sumofDigit.java
@@ -0,0 +1,12 @@
+public class sumofDigit {
+    public static void main(String[] args) {
+        int n=123;
+        int sum=0;
+        while(n!=0){
+            int digit=n%10;
+            sum+=digit;
+            n=n/10;
+        }
+        System.out.println(sum);
+    }
+}
