970219f25c152ff5a41bb02c5b7c337a66fb5c55
somnathsingh712 <somnathsingh9517@gmail.com>
2026-05-21 16:25:46 +0530

    Remove spaces from String

---
+++ removeSpacesFromString.java
@@ -0,0 +1,7 @@
+public class removeSpacesFromString {
+    public static void main(String[] args) {
+        String str="Ram is a good boy";
+        str=str.replaceAll(" ","");
+        System.out.println(str);
+    }
+}
