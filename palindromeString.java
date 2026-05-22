a171931daddd70591f44ede2754cde782e68d29b
somnathsingh712 <somnathsingh9517@gmail.com>
2026-05-22 17:47:29 +0530

    pallindrome in String

---
+++ palindromeString.java
@@ -0,0 +1,23 @@
+
+
+import java.util.Scanner;
+
+class palindromeString{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+
+        String rev="";
+        for(int i=str.length()-1;i>=0;i--){
+            rev=rev + str.charAt(i);
+        }
+
+        if(str.equals(rev)){
+            System.out.println("Pallindrome");
+        }
+        else{
+            System.out.println("Not Pallindrome");
+        }
+
+    }
+}
