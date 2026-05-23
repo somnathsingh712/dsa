646a5c453f8d9e27f95cc75baab33dc0be00fd95
somnathsingh712 <somnathsingh9517@gmail.com>
2026-05-23 12:18:59 +0530

    Power of a Number

---
+++ powerOfNumber.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class powerOfNumber {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int base=sc.nextInt();
+
+        int exp=sc.nextInt();
+
+        int result=1;
+        for(int i=0;i<exp;i++){
+            result=result*base;
+        }
+        System.out.println(result);
+    }
+}
