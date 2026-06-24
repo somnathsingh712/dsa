a06469bac3c2f4aca1dc59e6dbf02781c26490c4
somnathsingh712 <somnathsingh9517@gmail.com>
2026-06-24 12:12:46 +0530

    difference between star and hash

--- countStarAndHash.java
+++ countStarAndHash.java
@@ -1,16 +1,26 @@
+import java.util.Scanner;
+
 public class countStarAndHash {
-    String str="*#*#*#";
-    count1=0;
-    count2=0;
+
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+    String str= sc.next();
+    int star= 0;
+    int hash= 0;
     for(int i=0;i<str.length();i++){
         if(str.charAt(i)=='*'){
-            count1++;
+            star++;
         }
-        else{
-            count2++;
+        else if(str.charAt(i)=='#'){
+            hash++;
         }
-        System.out.println(count1-count2);
     }
+    System.out.println(star-hash);
+    }
+}
+
     
 
-}
+
+
