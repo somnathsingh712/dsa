e17598f04707fd833f922d1d76578bcf1e194359
somnathsingh712 <somnathsingh9517@gmail.com>
2026-08-02 18:01:12 +0530

    pyramid of number

---
+++ virtusa/pyramid.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class pyramid{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter number: ");
+        int n=sc.nextInt();
+        // for(int i=1;i<=n;i++){
+        //     for(int j=1;j<=n-i;j++){
+        //         System.out.print("  ");
+        //     }
+
+        //     for(int j=i;j>=1;j--){
+        //         System.out.print(j+" ");
+        //     }
+
+        //     for(int j=2;j<=i;j++){
+        //         System.out.print(j+" ");
+        //     }
+        //     System.out.println();
+        // }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print("  ");
+            }
+
+            for(int j=i;j>=1;j--){
+                System.out.print(j+" ");
+            }
+
+            for(int j=2;j<=i;j++){
+                System.out.print(j+" ");
+            }
+
+            System.out.println();
+        }
+    }
+}
