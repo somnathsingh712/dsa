a46a55fcb527d3e8811347c745e3ee6acda8e6eb
somnathsingh712 <somnathsingh9517@gmail.com>
2026-08-04 07:25:21 +0530

    AP series

---
+++ virtusa/apSeries.java
@@ -0,0 +1,9 @@
+public class apSeries {
+    public static void main(String[] args) {
+        int d=2;
+        int n=4;
+        int a=2;
+        int ap=((2*a) + (n-1)*d)*n/2;
+        System.out.println(ap);
+    }
+}
