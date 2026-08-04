e3e7fd129f3f5efc5f6e86e00ee4d97d98cca262
somnathsingh712 <somnathsingh9517@gmail.com>
2026-08-04 07:48:42 +0530

    Reverse String

---
+++ virtusa/reverseString.java
@@ -0,0 +1,8 @@
+public class reverseString {
+    public static void main(String[] args) {
+        String str="ramayana";
+        for(int i=str.length()-1;i>=0;i--){
+            System.out.print(str.charAt(i));
+        }
+    }
+}
